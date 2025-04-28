46a9eab27f355cce3279ddf14722993dbf5a85ec
matteo00gm <matteo00gm@gmail.com>
2025-04-28 11:00:24 +0200

    arrays

--- arrays/sum.go
+++ arrays/sum.go
@@ -6,3 +6,26 @@ func Sum(numbers []int) (result int) {
 	}
 	return result
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// The tail of a collection is all items in the collection except the first one (the "head").
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
